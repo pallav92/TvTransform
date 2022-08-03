7115d027dc3dcff93437b7250ed75947a9908c2f
Pallav.srivastava <pallav.srivastava@inmobi.com>
2022-08-03 13:08:53 +0530

    initial commit

---
+++ app/src/main/java/com/tvolution/tvtransformer/PlayerActivity.kt
@@ -0,0 +1,13 @@
+package com.tvolution.tvtransformer
+
+import android.app.Activity
+import android.os.Bundle
+
+class PlayerActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_activity_player)
+    }
+
+}
